977a36045f360a67cb14a468410899986b893d8a
jenleap <jenleap@gmail.com>
2023-10-14 14:01:37 -0400

    test 1

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const app = express();
 app.get('/', (req, res) => {
     console.log("Hello user.");
     res.status(200);
-    res.json({ message: "Hello from your List app."});
+    res.json({ message: "Hello from your List app. Test 1."});
     return res;
 });
 
