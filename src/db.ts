09a99776731298c34c2b356d676e2c3bc69aa7c8
jenleap <jenleap@gmail.com>
2023-10-18 10:32:03 -0400

    update datasource

--- src/db.ts
+++ src/db.ts
@@ -1,15 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 import config from './config';
 
-console.log(config.dbUrl);
-
 const prisma = new PrismaClient({
     datasources: {
       db: {
         url: config.dbUrl,
       },
     },
-    datasourceUrl: config.dbUrl
   });
 
 export default prisma;
